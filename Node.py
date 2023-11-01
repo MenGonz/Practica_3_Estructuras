d8cea5018e14f26873c9d5e9b8174e1413c41a35
MenGonz <semendozag@unal.edu.co>
2023-11-01 17:50:34 -0500

    Clase Tree y Node terminadas
    Fin de la implementación.
    Está todo funcionando melito.
    Queda faltando solamente crear un método que printee el arbol de una manera estética pero no sé cómo hacerlo.

--- Node.py
+++ Node.py
@@ -10,31 +10,46 @@ class Node:
         self.p: Node | None = None
         
     
-    #privados
+    #Méodos privados de la clase
     def __insert_r(self, data):
-        if self.r is None:
-            self.r = Node(data)
+        if not self.has_right():
+            n: Node = Node(data) 
+            self.r = n
+            n.p = self
         else:
             self.r.insert(data)
             
     def __insert_l(self, data):
-        if self.l is None:
-            self.l = Node(data)
+        if not self.has_left():
+            n: Node = Node(data) 
+            self.l = n
+            n.p = self
         else:
             self.l.insert(data)
     
     
     #métodos varios
     def insert(self, data):
+        """
+        Le inserta un hijo al subárbol apropiado debajo de el nodo.
+        """
         if self.data > data:
             self.__insert_l(data)
         else:
             self.__insert_r(data)
-            
-    def is_leaf(self) -> bool:
-        return self.get_right() is None and self.get_left() is None 
-        
+               
+    def has_left(self) -> bool: return bool(self.get_left())
+    
+    def has_right(self) -> bool: return bool(self.get_right())    
+    
+    def is_leaf(self) -> bool: return not( self.has_left() or self.has_right()) 
+    
+    def is_root(self) -> bool: return not bool(self.get_parent())
+    
     def leftmost_node_of_right_subtree(self):
+        """
+        Retorna el descendiente más pequeño del sub-árbol derecho del nodo.
+        """
         temp: Node= self.get_right()
         while temp.get_left():
             temp = temp.get_left()
@@ -42,6 +57,9 @@ class Node:
         return temp
     
     def rightmost_node_of_left_subtree(self):
+        """
+        Retorna el descendiente más grande del sub-árbol derecho del nodo.
+        """
         temp: Node = self.get_left()
         while temp.get_right():
             temp = temp.get_right()
@@ -49,48 +67,41 @@ class Node:
         return temp
     
     
+    
     #Getters y Setters
     
     def get_height(self) -> int:
-        if self.r is None and self.l is None:
+        
+        if self.is_leaf():
             return 1
-        elif self.r is None:
-            return 1 + self.l.get_height()
-        elif self.l is None:
-            return 1 + self.r.get_height()
+        elif not self.has_right():
+            return 1 + self.get_left().get_height()
+        elif not self.has_left():
+            return 1 + self.get_right().get_height()
         else:
             return 1 + max(self.r.get_height(), self.l.get_height())
         
     def get_depth(self) -> int:
-        if self.p is None:
+        if self.is_root():
             return 0
         else:
             return 1 + self.p.depth()
         
-    def get_data(self):
-        return self.data
-    def set_data(self, data):
-        self.data = data
+    def get_data(self): return self.data
+    def set_data(self, data): self.data = data
     
-    def get_parent(self):
-        return self.p
-    def set_parent(self,nod):
-        self.p = nod
+    def get_parent(self): return self.p
+    def set_parent(self,nod): self.p = nod
     
-    def get_right(self):
-        return self.r
-    def set_right(self,nod):
-        self.r = nod
+    def get_right(self): return self.r
+    def set_right(self,nod): self.r = nod
     
-    def get_left(self):
-        return self.l
-    def set_left(self,nod):
-        self.l = nod
+    def get_left(self): return self.l
+    def set_left(self,nod): self.l = nod
     
     
     #Métodos dunder
     def __str__(self):
-        return str(f"({self.data})")
-    
+        return str(self.get_data())
     
     
--- Tree.py
+++ Tree.py
@@ -16,25 +16,35 @@ class Tree:
             self.root = None
             self.height = 0
             self.size = 0
-            
+        
+        
+    def get_height(self): return self.height
+    
+    def get_root(self): return self.root
+    
+    def get_size(self): return self.size
+    
+    def is_empty(self): return not bool(self.get_root())
+    
     def insert(self, data):
-        if self.root is None:
+        """
+        Inserta una data en el árbol respetando la estructura.
+        """
+        if self.is_empty():
             self.root = Node(data)
-            self.height = 1
         else:
-            self.root.insert(data)
-            self.height = self.root.height()
+            self.get_root().insert(data)
+        
+        self.height = self.root.get_height()
         self.size += 1
         
         
-    def get_height(self):
-        return self.height
-    
-    def get_root(self):
-        return self.root.get_data()
-    
     def search(self, data) -> Node:
-        temp = self.root
+        """
+        Busca un dato en el árbol y retorna (si existe) el nodo que lo contiene.
+        """
+        temp: Node = self.get_root()
+        
         while temp:
             if temp.get_data() == data:
                 return temp
@@ -42,30 +52,107 @@ class Tree:
                 temp = temp.get_left()
             else:
                 temp = temp.get_right()
+                
         return None
     
     
-    def delete(self, data):
-        nodo = self.search(data)
+    def remove(self, data):
+        """
+        Recibe un dato y remueve del árbol el nodo al que le corresponde.
+        """
+        nodo: Node = self.search(data)
         
         if nodo:
-            if nodo.is_leaf():
+            if nodo.is_leaf() and not nodo.is_root():
                 if nodo.get_parent().get_left() == nodo:
                     nodo.get_parent().l = None
-                    nodo.p = None
-                else:
+                    nodo.p = None    
+                elif nodo.get_parent().get_right() == nodo:
                     nodo.get_parent().r = None
                     nodo.p = None
-                    
-            elif nodo.get_left() is not None:
-                temp = nodo.rightmost_node_of_left_subtree()
+            elif nodo.has_left():
+                temp: Node = nodo.rightmost_node_of_left_subtree()
                 nodo.data, temp.data = temp.data, nodo.data
-                self.delete(temp)
-            else:
-                temp = nodo.leftmost_node_of_right_subtree()
+                self.remove(temp)
+            elif nodo.has_right():
+                temp: Node = nodo.leftmost_node_of_right_subtree()
                 nodo.data, temp.data = temp.data, nodo.data
-                self.delete(temp)
-                
+                self.remove(temp)
+        
+            self.size -= 1
+            self.height = self.get_root().get_height()
     
-                
+    
+    
+    def traverse(self, orden:str ="in") -> tuple[str]:
+        """
+        Retorna tupla con recorrido del árbol en el orden ord especificado.
+        
+        ord = "pre" | "in" | "pos" 
+        """
+        
+        root: Node = self.get_root()
+        recorrido: list[str] = []
+        
+        def visit(node: Node): recorrido.append(node.get_data())
+        
+        #funciones recursivas para recorrer el árbol
+        def preorder(nodo: Node):
+            """Recorrido RID"""
+            visit(nodo)
+            if nodo.has_left():
+                preorder(nodo.get_left())
+            if nodo.has_right():
+                preorder(nodo.get_right())
+        
+        def inorder(nodo: Node):
+            """Recorrido IRD"""
+            if nodo.has_left():
+                inorder(nodo.get_left())
+            visit(nodo)
+            if nodo.has_right():
+                inorder(nodo.get_right())
+        
+        def posorder(nodo: Node):
+            """Recorrido IDR"""
+            if nodo.has_left():
+                posorder(nodo.get_left())
+            if nodo.has_right():
+                posorder(nodo.get_right())
+            visit(nodo)
+        
+        assert orden in {"in", "pre", "pos"}, "orden incorrecto"
+        
+        if orden == "pos":
+            posorder(root)
+        elif orden == "pre":
+            preorder(root)
+        else:
+            inorder(root)
+        
+        return tuple(recorrido)
+        
+        
+    def __str__(self):
+        return f"""
+Arbol binario de busqueda
+    root: {self.get_root()}
+    size: {self.get_size()}
+    height: {self.get_height()}
+    nodos: {self.traverse()}
+                """
+        
+        
+    
+if __name__ == "__main__":
+    T: Tree = Tree()
+    
+    T.insert(2)
+    T.insert(1)
+    T.insert(3)
+    T.insert(4)
+    T.insert(0)
+    
+    
+    print(T)
     
