9006eb533b278f402416f8c1f4b86b5e775967e6
MenGonz <semendozag@unal.edu.co>
2023-11-10 17:18:42 -0500

    ya solo falta hacer display del árbol

--- User.py
+++ User.py
@@ -6,5 +6,11 @@ class User():
     def __init__(self, nombre: str, identificacion: int):
         self.nombre = nombre
         self.identificacion = identificacion
+        
+    def get_id(self) -> int:
+        return self.identificacion
+
+    def get_key(self) -> int:
+        return sum(list(map(int,list(str(self.identificacion)))))
     
     
--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from random import randint
 
 if __name__ == "__main__":
     T: Tree = Tree()
-    for _ in range(10):
-        r = randint(0,100)
-        T.insert(r, User("Juan", r))
+    usuarios = [User("juan",10101013),
+                User("Pablo",10001011),
+                User("Maria",10101015),
+                User("Ana",1010000),
+                User("Diana",10111105),
+                User("Mateo",10110005)]
+    
+    for u in usuarios:
+        T.insert(u.get_key(), u)
         
-    print(T)
+    
+    #print(T)
+    print(T.traverse("in"))
